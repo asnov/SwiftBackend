e830eb292c78ee00e6e58d7a14c70402fd40fd70
Alex <mail@gmail.com>
2023-09-19 10:19:25 -0300

    added routing;

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,3 +12,29 @@ try await onIncomingRequest { req, res in
         let text = try await fetchResponse.text()
         try await res.status(200).send(text)
 }
+
+let router = Router()
+
+router.get("/status") { req, res in
+    try await res.status(.ok).send("OK")
+}
+
+router.get("/user/:name") { req, res in
+    let name = req.pathParams["name"] ?? ""
+    let text = "Hello, \(name)!"
+    try await res.status(.ok).send(text)
+}
+
+
+
+struct User: Codable {
+    let name: String
+}
+
+// Type of expression is ambiguous without more context
+//router.post("/user") { req, res in
+//    let user = try await req.body.decode(User.self)
+//    try await res.status(.created).send(user)
+//}
+
+try await router.listen()
