821d87e1cd38441dd342ad3a09631b351d4a193b
Alex <mail@gmail.com>
2023-09-19 10:10:11 -0300

    added Sample App;

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Compute
+
 
 print("Hello, world!")
+
+try await onIncomingRequest { req, res in
+    let fetchResponse = try await fetch("https://httpbin.org/json", .options(
+            headers: ["user-agent": "swift-compute-runtime"]
+        ))
+        let text = try await fetchResponse.text()
+        try await res.status(200).send(text)
+}
