e7b229b86a5652cd92da002fb8d4d9e20c152733
Alex <mail@gmail.com>
2023-09-19 10:29:39 -0300

    removed onIncomingRequest as we use router;

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,13 +5,13 @@ import Compute
 
 print("Hello, world!")
 
-try await onIncomingRequest { req, res in
-    let fetchResponse = try await fetch("https://httpbin.org/json", .options(
-            headers: ["user-agent": "swift-compute-runtime"]
-        ))
-        let text = try await fetchResponse.text()
-        try await res.status(200).send(text)
-}
+//try await onIncomingRequest { req, res in
+//    let fetchResponse = try await fetch("https://httpbin.org/json", .options(
+//            headers: ["user-agent": "swift-compute-runtime"]
+//        ))
+//        let text = try await fetchResponse.text()
+//        try await res.status(200).send(text)
+//}
 
 let router = Router()
 
